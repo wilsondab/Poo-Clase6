7b58645976088503f5f5a71116a58c0c5193db74
GestionTalentoCDA <gestiondetalento@cdainfo.com>
2024-05-29 16:59:13 -0300

    Ejercicio3Modulo2

---
+++ EjercicioClase3Modulo2EFCore/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace EjercicioClase3Modulo2EFCore
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            #region Pasos previos
+            //Ejecutar el script de base de datos en Management Studio para crear la base de datos y la tabla con datos
+            //Instalar Microsoft.EntityFrameworkCore y Microsoft.EntityFrameworkCore.SqlServer
+            //Crear las entidades necesarias
+            //Crear el dbcontext
+            //Configurar aqui el connection string e instanciar el contexto de la base de datos.
+            #endregion
+
+            #region ejercicio 1
+            //Obtener un listado de todos los actores y actrices de la tabla actor
+
+            #endregion
+
+            #region ejercicio 2
+            //Obtener listado de todas las actrices de la tabla actor
+
+            #endregion
+
+            #region ejercicio 3
+            //Obtener un listado de todos los actores y actrices mayores de 50 años de la tabla actor
+
+            #endregion
+
+            #region ejercicio 4
+            //Obtener la edad de la actriz "Julia Roberts"
+
+            #endregion
+
+            #region ejercicio 5
+            //Insertar un nuevo actor en la tabla actor con los siguientes datos:
+            //nombre: Ricardo
+            //apellido: Darin
+            //edad: 67 años
+            //nombre_artistico: Ricardo Darin
+            //nacionalidad: argentino
+            //género: Masculino.
+
+            #endregion
+
+            #region ejercicio 6
+            //obtener la cantidad de actores y actrices que no son de Estados Unidos.
+
+            #endregion
+
+            #region ejercicio 7
+            //obtener los nombres y apellidos de todos los actores maculinos.
+            #endregion
+        }
+    }
+}
